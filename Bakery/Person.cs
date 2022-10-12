9e713910d2299eee1010179f0a4fcaf4e3b14a4d
Eduard Ghergu <ghergu@yahoo.com>
2022-10-12 19:11:39 +0300

    Update Person.cs

--- Bakery/Person.cs
+++ Bakery/Person.cs
@@ -2,13 +2,7 @@ namespace Bakery;
 
 public abstract class Person {
 
- // private static Person MakeValidClickHelpConfiguration()
- // {
- //  var clickHelpConfigurationBuilder = new ClickHelpConfigurationBuilder();
- //  clickHelpConfigurationBuilder.SetProjectUri(new Uri(Constants.ValidProjectUrl));
- //  return clickHelpConfigurationBuilder.GetResult();
- // }
- //
+
 /*
  // Builder Design Pattern
  - pas 1
